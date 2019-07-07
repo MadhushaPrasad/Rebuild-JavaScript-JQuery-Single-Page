6043235c30fba87c10939b6d9c9d4bb3aa6a4c03
Madhusha Prasad <madushaprasad21@gmail.com>
2019-07-08 01:57:52 +0530

    Change CustomerController

--- api/controller/CustomerController.js
+++ api/controller/CustomerController.js
@@ -1,22 +1,35 @@
-function addTableElement(id,name,address,tel) {
-    var il = document.createElement("i");
-    il.classList.add("fas fa-edit btnedit");
-    var il2 = document.createElement("i");
-    il2.classList.add("fas fa-edit btnedit");
-    var tblRow = "<tr>"+"<td>"+id+"</td>"+"<td>"+name+"</td>"+"<td>"+address+"</td>"+"<td>"+tel+"</td>"+"<td>"+i1+"</td>"+"<td>"+il2+"</td>"+"</tr>";
-    $('#tbl-Customer').append(tblRow);
-
-// <td><i class="fas fa-edit btnedit"></i></td>
-//     <td><i class="fas fa-trash-alt btnedelte"></i></td>
+function addCustomer(id, name, address, tel) {
+    console.log(id);
+    console.log(name);
+    console.log(address);
+    var c1 = new CustomerDTO(id, name, address, tel);
+    customerTable.push(c1);
 }
-$('#btnRegister').click(function () {
-    var id = $('#customerID').val();
-    var name = $('#customerName').val();
-    var address = $('#customerAddress').val();
-    var tp = $('#customerTP').val();
-    addTableElement(id,name,address,tp);
+
+function getAllCustomer() {
+    $("#tbl-Customer").empty();
+    for (var i in customerTable) {
+        var id = customerTable[i].getCustomerID();
+        var name = customerTable[i].getCustomerName();
+        var address = customerTable[i].getCustomerAddress();
+        var tp = customerTable[i].getCustomerTP();
+
+        var tblRow = "<tr>" +
+            "<td>" + id + "</td>" +
+            "<td>" + name + "</td>" +
+            "<td>" + address + "</td>" +
+            "<td>" + tp + "</td>" +
+            +"</tr>";
+        $("#tbl-Customer").append(tblRow);
+    }
+}
+
+$("#btnRegister").click(function () {
+    var id = $("#customerID").val();
+    var name = $("#customerName").val();
+    var address = $("#customerAddress").val();
+    var tp = $("#customerTP").val();
+    addCustomer(id, name, address, tp);
+    getAllCustomer();
 });
 
-// $('#btnDeleteAll').click(function () {
-//     $('#tbl-Customer').clear();
-// });
